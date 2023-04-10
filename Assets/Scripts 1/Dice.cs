1635b73cb292d70e619539f7dcae1b84b012e7ab
ZinuoXu <99519288+ZinuoXu@users.noreply.github.com>
2023-04-09 22:02:14 -0400

    Stuff
    Today we replaced the code of Dice with a newer version, fixed several bugs in the code, and stopped the bug that allows the dice to fly away.

--- Assets/Scripts 1/Dice.cs
+++ Assets/Scripts 1/Dice.cs
@@ -1,87 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class Dice : MonoBehaviour
 {
     Rigidbody rb;
-
-    bool hasLanded, thrown;
-    PlayerMoveScript plMove;
+ 
+    [SerializeField] public bool hasLanded, thrown;
+    public PlayerMoveScript plMove;
+ 
+ 
     Vector3 initPosition;
-    public int DiceValue;
+ 
+     int diceValue;
     public DiceSide[] diceSides = new DiceSide[6];
-
+ 
     public int GetDiceValue()
     {
-        return DiceValue;
+        return diceValue;
     }
-
-
-    private void RollDice()
-    {
-        if(!thrown && !hasLanded)
-        {
-            thrown = true;
-            rb.useGravity = true;
-            rb.AddTorque(Random.Range(0,500), Random.Range(0,500), Random.Range(0,500));
-        }
-        else if(thrown && hasLanded)
-        {
-            Reset();
-        }
-    }
-    private void Reset()
-    {
-        transform.position = initPosition;
-        rb.useGravity = false;
-        thrown = false;
-        hasLanded = false;
-        plMove.steps = 0;
-    }
-
-    private void RollAgain()
-    {
-        Reset();
-        thrown = true;
-        rb.useGravity = true;
-        rb.AddTorque(Random.Range(0,500), Random.Range(0,500), Random.Range(0,500));
-        
-    }
-    private void DiceValCheck()
-    {
-        DiceValue = 0;
-        foreach(DiceSide side in diceSides)
-        {
-            if(side.OnGround())
-            {
-                DiceValue = side.sideValue;
-                Debug.Log(DiceValue + "has been rolled.");
-            }
-        }
-    }
-
+ 
+ 
+ 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         initPosition = transform.position;
         rb.useGravity = false;
     }
-
-    private void OnClick()
+ 
+    public void OnClick()
     {
-       
         RollDice();
-
-        if(rb.IsSleeping() && !hasLanded && thrown)
+    }
+    private void Update()
+    {
+        if (rb.IsSleeping() && !hasLanded && thrown)
         {
             hasLanded = true;
             rb.useGravity = false;
-            DiceValCheck();
+            DiceValueCheck();
+ 
         }
-        else if(rb.IsSleeping() && hasLanded && DiceValue == 0)
+        else if (rb.IsSleeping() && hasLanded && diceValue == 0)
         {
             RollAgain();
+        }
+    }
+ 
+    void RollDice()
+    {
+        if(!thrown && !hasLanded)
+        {
+            thrown = true;
+            rb.useGravity = true;
+            rb.AddTorque(Random.Range(0, 500), Random.Range(0, 500), Random.Range(0, 500));
+        }
+        else if(thrown && hasLanded)
+        {
+            Reset();
+        }
+    }
+ 
+    private void Reset()
+    {
+        transform.position = initPosition;
+        thrown = false;
+        hasLanded = false;
+        rb.useGravity = false;
+    }
+    private void RollAgain()
+    {
+ 
+        Reset();
+        thrown = true;
+        rb.useGravity = true;
+        rb.AddTorque(Random.Range(0, 500), Random.Range(0, 500), Random.Range(0, 500));
+ 
+ 
+    }
+ 
+    void DiceValueCheck()
+    {
+        diceValue= 0;
+        foreach(DiceSide side in diceSides)
+        {
+            if(side.OnGround())
+            {
+                diceValue = side.sideValue;
+                Debug.Log(diceValue + " has been rolled");
+                plMove.OnRoll();
+                Reset(); 
+            }
         }
     }
 }
--- Assets/Scripts 1/PlayerMoveScript.cs
+++ Assets/Scripts 1/PlayerMoveScript.cs
@@ -1,31 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class PlayerMoveScript : MonoBehaviour
 {
     public GameRoute currentRoute;
     int routePosition;
     public int steps = 0;
     bool isMoving;
-    Dice dice1, dice2;
-
+    public Dice dice1, dice2;
+ 
     public void OnRoll()
     {
         steps += dice1.GetDiceValue()
             + dice2.GetDiceValue();
         StartCoroutine(Move());
     }
-
-    // private void Update()
-    // {
-    //     if(!isMoving && Input.GetKeyDown(KeyCode.LeftShift))
-    //     {
-    //         steps = Random.Range(1, 7);
-    //         Debug.Log("Dice Rolled" + steps);
-    //         StartCoroutine(Move());
-    //     }
-    // }
+ 
+    private void Update()
+    {
+        if(dice1.hasLanded && dice2.hasLanded && !isMoving)
+        {
+            Debug.Log("Dice Rolled " + steps);
+            StartCoroutine(Move());
+        }
+    }
+ 
     public IEnumerator Move()
     {
         if (isMoving)
@@ -33,24 +33,25 @@ public class PlayerMoveScript : MonoBehaviour
             yield break;
         }
         isMoving = true;
-
+ 
         while(steps > 0)
         {
             routePosition++;
             routePosition %= currentRoute.childrenNodeList.Count;
-            Vector3 nextPos = currentRoute.childrenNodeList[routePosition +1].position;
+            Vector3 nextPos = currentRoute.childrenNodeList[routePosition + 1].position;
             while(MoveToNextNode(nextPos)) { yield return null; }
-
+ 
             yield return new WaitForSeconds(0.1f);
             steps--;
             routePosition++;
         }
+ 
         isMoving = false;
     }
-
+ 
+ 
     bool MoveToNextNode(Vector3 goal)
     {
         return goal != (transform.position = Vector3.MoveTowards(transform.position, goal, 2f * Time.deltaTime));
     }
-
 }
