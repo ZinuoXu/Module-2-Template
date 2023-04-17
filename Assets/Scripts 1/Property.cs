57fad3d7a94a6cfab6b6250abd9826f6c7ee1b11
ZinuoXu <99519288+ZinuoXu@users.noreply.github.com>
2023-04-16 21:58:55 -0400

    Stuff and things
    Today we summarized the different functions of properties and its manager, we then created the functions and the list of the properties.

---
+++ Assets/Scripts 1/Property.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Property : MonoBehaviour
+{
+
+    public bool isGpBought;
+    public int initialRent;
+    public int NumHouses;
+}
---
+++ Assets/Scripts 1/PropertyManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropertyManager
+{
+    public string Name;
+    public string Description;
+    public int price;
+    public bool isBought = false;
+    public int index;
+    public int playerIndex;
+
+
+    public void Buy()
+    {
+
+    }
+
+    public bool GetStatus()
+    {
+        return isBought;
+    }
+
+    public void GetRent()
+    {
+
+    }
+
+    public void SellToBank()
+    {
+
+    }
+}
